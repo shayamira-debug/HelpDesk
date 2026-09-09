d1263ca6d1bf7525ee74b4bd6b02b6a52153e94a
shayamira-debug <shayamira@gmail.com>
2026-09-09 19:44:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="maintenance-pwa-v3";
+const CACHE="maintenance-pwa-v4";
 const ASSETS=["./","./index.html","./portal.html","./admin.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
